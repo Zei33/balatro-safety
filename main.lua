5a3e53692dbedeb062f69abafe4d1c45de5e44b8
Zei33 <Zei33@users.noreply.github.com>
2024-05-02 00:09:50 +1000

    Created basic UI overlay with buttons.

---
+++ main.lua
@@ -0,0 +1,291 @@
+local logging = require("logging")
+local logger = logging.getLogger("balatro-safety")
+
+local function create_multiline_text(text, text_scale, alignment, padding)
+	local text = text or { }
+	for i, v in ipairs(text) do
+		text[i] = {
+			n = G.UIT.R,
+			config = {
+				align = alignment or "cm",
+				padding = padding or 0.05
+			},
+			nodes = {
+				{
+					n = G.UIT.T,
+					config = {
+						text = v,
+						scale = text_scale or 1,
+						colour = G.C.UI.TEXT_LIGHT,
+						shadow = true
+					}
+				}
+			}
+		};
+	end;
+
+	return text
+end
+
+local function create_UIBox_generic_container(options)
+    options = options or {
+		content = {}
+	}
+
+    return {
+		{
+			n = G.UIT.R,
+			config = {
+				align = "cm",
+				padding = 0.03,
+				colour = G.C.UI.TRANSPARENT_LIGHT,
+				r = 1
+			},
+			nodes = {
+				{
+					n = G.UIT.R,
+					config = {
+						align = "cm",
+						padding = 0.05,
+						colour = G.C.DYN_UI.MAIN,
+						r = 1
+					},
+					nodes = {
+						{
+							n = G.UIT.R,
+							config = {
+								align = "cm",
+								colour = G.C.DYN_UI.BOSS_DARK,
+								minw = options.width or 10,
+								minh = options.height or 4,
+								r = 1,
+								padding = 0.08
+							},
+							nodes = options.content
+						}
+					}
+				}
+			}
+		}
+	}
+end
+
+local function create_UIBox_confirmation(options)
+	options = options or {}
+
+	return {
+		{
+			n = G.UIT.C,
+			config = {
+				align = "cm",
+				padding = 0.05,
+				minw = 5,
+				minh = 4,
+				r = 1,
+				colour = G.C.GREEN
+			},
+		},
+		{
+			n = G.UIT.C,
+			config = {
+				align = "cr",
+				padding = 0,
+				minw = 5,
+				minh = 4,
+				r = 1,
+				colour = G.C.UI.BACKGROUND_DARK
+			},
+			nodes = {
+				{
+					n = G.UIT.R,
+					config = {
+						align = "cm",
+						padding = 0.025,
+						minw = 5,
+						minh = 4,
+						maxw = 5,
+						r = 1
+					},
+					nodes = {
+						{
+							n = G.UIT.R,
+							config = {
+								align = "cm",
+								padding = 0.3,
+								minw = 5,
+								minh = 2,
+								maxw = 5,
+								r = 1,
+								--colour = G.C.BLUE
+							},
+							nodes = create_multiline_text({
+								"Are you SURE",
+								"you want to do this?"
+							}, 0.75, "cm", 0.02, 1)
+						}
+					}
+				},
+				{
+					n = G.UIT.R,
+					config = {
+						align = "cm",
+						padding = 0.025,
+						minw = 5,
+						minh = 2,
+						maxw = 5,
+						r = 1
+					},
+					nodes = {
+						{
+							n = G.UIT.R,
+							config = {
+								align = "cm",
+								padding = 0.025,
+								minw = 5,
+								minh = 2,
+								maxw = 5,
+								r = 1,
+								--colour = G.C.RED
+							},
+							nodes = {
+								{
+									n = G.UIT.R,
+									config = {
+										align = "cm",
+										padding = 0.05,
+										minw = 4.75,
+										minh = 1,
+										maxw = 4.75,
+										r = 1
+									},
+									nodes = {
+										UIBox_button({
+											button = 'safety_confirm', 
+											label = { "CONFIRM" },
+											minw = 4.75, 
+											minh = 0.95, 
+											scale = 0.5, 
+											colour = G.C.GREEN,
+											id = 'safety_confirm'
+										})
+									}
+								},
+								{
+									n = G.UIT.R,
+									config = {
+										align = "cm",
+										padding = 0.05,
+										minw = 4.75,
+										minh = 1,
+										maxw = 5,
+										r = 1
+									},
+									nodes = {
+										UIBox_button({
+											button = 'safety_cancel', 
+											label = { "CANCEL" },
+											minw = 4.75, 
+											minh = 0.95, 
+											scale = 0.5, 
+											colour = G.C.RED,
+											id = 'safety_cancel'
+										})
+									}
+								},
+								{
+									n = G.UIT.R,
+									config = {
+										align = "cm",
+										padding = 0,
+										minw = 4.5,
+										minh = 0.1,
+										maxw = 5,
+										r = 1
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+end
+
+local function on_enable()
+	G.UIDEF.safety_box = function()
+		return {
+			n = G.UIT.ROOT,
+			config = {
+				align = "cm",
+				padding = 0.1,
+				r = 2
+			},
+			nodes = create_UIBox_generic_container({
+				content = create_UIBox_confirmation()
+			})
+		}
+	end
+
+	G.FUNCS.overlay_menu({definition = G.UIDEF.safety_box()})
+end
+
+local function on_disable()
+end
+
+local function menu()
+end
+
+local function on_game_load(args)
+end
+
+local function on_game_quit()
+end
+
+local function on_key_pressed(key)
+end
+
+local function on_key_released(key)
+end
+
+local function on_mouse_pressed(button, x, y, touch)
+end
+
+local function on_mouse_released(button, x, y)
+end
+
+local function on_mousewheel(x, y)
+end
+
+local function on_pre_render()
+end
+
+local function on_post_render()
+end
+
+local function on_error(message)
+end
+
+local function on_pre_update(dt)
+end
+
+local function on_post_update(dt)
+end
+
+return {
+    on_enable = on_enable,
+    on_disable = on_disable,
+    menu = menu,
+    on_game_load = on_game_load,
+    on_game_quit = on_game_quit,
+    on_key_pressed = on_key_pressed,
+    on_key_released = on_key_released,
+    on_mouse_pressed = on_mouse_pressed,
+    on_mouse_released = on_mouse_released,
+    on_mousewheel = on_mousewheel,
+    on_pre_render = on_pre_render,
+    on_post_render = on_post_render,
+    on_error = on_error,
+    on_pre_update = on_pre_update,
+    on_post_update = on_post_update,
+}
